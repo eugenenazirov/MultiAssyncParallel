d464db2866ab8de5a59036b3af92138e68622b0c
Eugene Nazirov <jagzonmusic@gmail.com>
2022-06-10 21:31:48 +0600

    fixed DeprecationWarning

--- asyncio/05_03_task_wait.py
+++ asyncio/05_03_task_wait.py
@@ -1,8 +1,6 @@
-# this code doesn't work correctly, there are an exception with dunder __aenter__
-
-
-import asyncio, aiohttp
-from asyncio import FIRST_COMPLETED
+import socket
+import asyncio
+import aiohttp
 from collections import namedtuple
 
 
@@ -15,7 +13,8 @@ services = (
 
 
 async def get_json(url):
-    async with aiohttp.ClientSession as session:
+    conn = aiohttp.TCPConnector(family=socket.AF_INET, ssl=True, )
+    async with aiohttp.ClientSession(connector=conn) as session:
         async with session.get(url) as response:
             return await response.json()
 
@@ -29,10 +28,12 @@ async def fetch_ip(service):
 
 
 async def main():
-    coros = [fetch_ip(service) for service in services]
-    coro1 = coros[0]
-    coro2 = coros[1]
-    await asyncio.gather(coro1, coro2)
+    tasks = set()
+    for service in services:
+        task = asyncio.create_task(fetch_ip(service))
+        tasks.add(task)
+
+    pending, done = await asyncio.wait(tasks)
 
     for x in done:
         print(x.result())
